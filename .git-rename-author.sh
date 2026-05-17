f5b86c27ebe83e58d58c08f230dfe34e36fb8d06
muhammadimranrafique41-code <muhammadimranrafique41-code@gmail.com>
2026-05-17 12:39:38 +0500

    code update

---
+++ .git-rename-author.sh
@@ -0,0 +1,6 @@
+if [ "$GIT_AUTHOR_NAME" = "smahveenrafique-rgb" ]; then
+    export GIT_AUTHOR_NAME="muhammadimranrafique41-code"
+fi
+if [ "$GIT_COMMITTER_NAME" = "smahveenrafique-rgb" ]; then
+    export GIT_COMMITTER_NAME="muhammadimranrafique41-code"
+fi
